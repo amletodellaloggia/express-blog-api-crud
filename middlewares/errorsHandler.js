62ff53f7e93adddc0676a973f42fbb018f28f3c2
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-07 09:44:19 +0200

    [upd] edited error code

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,7 +1,7 @@
 // Definisco il middleware errorsHandler, per la gestione degli errori
 const errorsHandler = (err, req, res, next) => {
 	console.log(err);
-	res.status(404).json({
+	res.status(500).json({
 		error: err.message
 	});
 };
